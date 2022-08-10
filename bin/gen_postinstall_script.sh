e193e7c27aa6d99526fe25665ebdf210fb8c062f
kriipke <kriipke@pm.me>
2022-08-10 09:35:42 -0400

    Create gen_postinstall_script.sh

---
+++ bin/gen_postinstall_script.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# prints to standard out the concatentation
+# of these files, so the commands can be run
+# from a script in the order that Packer would
+# execute them during bootstrapping
+
+cat \
+../template/scripts/motd.sh \
+../template/scripts/issue.sh \
+../template/scripts/metadata.sh \
+../template/scripts/sshd.sh \
+../template/scripts/networking.sh \
+../template/scripts/configure_vim.sh \
+../template/scripts/install_fzf.sh \
+../template/scripts/install_nnn.sh \
+../template/scripts/configure_zsh.sh \
+../template/scripts/configure_tmux.sh \
+../template/scripts/install_bat.sh \
+../template/scripts/install_croc.sh \
+../template/scripts/install_fd.sh \
+../template/scripts/install_lnav.sh \
+../template/scripts/install_rg.sh \
+../template/scripts/install_z.sh \
+../template/scripts/cleanup.sh \
+../template/scripts/minimize.sh
