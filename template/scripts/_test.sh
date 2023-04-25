af8712235f1a4494848c97f8499b451b2c045c5b
Spencer Smolen <45985425+kriipke@users.noreply.github.com>
2023-04-25 19:33:58 -0400

    Create _test.sh
    created to test scripts in the order they are meant to be run in

---
+++ template/scripts/_test.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# ORDER SCRIPTS ARE TO BE RUN IN:
+
+#./motd.sh
+#./issue.sh
+#./metadata.sh
+#./sshd.sh
+#./networking.sh
+#./configure_vim.sh
+#./configure_zsh.sh
+#./install_fzf.sh
+#./install_nnn.sh
+#./configure_tmux.sh
+#./install_bat.sh
+#./install_croc.sh
+#./install_fd.sh
+#./install_lnav.sh
+#./install_rg.sh
+#./install_z.sh
+#./cleanup.sh
+#./minimize.sh
