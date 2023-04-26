d5913f3ab05f8fc803858f0948d06763d9aa7538
Spencer Smolen <45985425+kriipke@users.noreply.github.com>
2023-04-25 22:31:16 -0400

    Update install_z.sh

--- template/scripts/install_z.sh
+++ template/scripts/install_z.sh
@@ -10,6 +10,13 @@
 Z_RELEASE='1.11'
 Z_REPO='https://github.com/rupa/z'
 
+# Configure debugging
+set -eux
+set -o pipefail
+command -v bc || dnf install -y bc
+command -v tput || dnf install -y ncurses
+PS4='$(tput setaf 4)$(printf "%-12s\\t%.3fs\\t@line\\t%-10s" $(date +%T) $(echo $(date "+%s.%3N")-'$(date "+%s.%3N")' | bc ) $LINENO)$(tput sgr0)'
+
 Z_REPO_ROOT="https://raw.githubusercontent.com/rupa/z/v$Z_RELEASE"
 Z_TARGET_DIR=/usr/share/z
 TEST_CMD="test -f $HOME/.z"
