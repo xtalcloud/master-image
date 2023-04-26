1c5b2ae65527307b4d7de1d8e36af1d26e41259c
Spencer Smolen <45985425+kriipke@users.noreply.github.com>
2023-04-25 22:27:11 -0400

    added debug configuration to sshd.sh

--- template/scripts/sshd.sh
+++ template/scripts/sshd.sh
@@ -1,7 +1,16 @@
-#!/bin/sh -eux
+#!/bin/sh
+
+# Configure debugging
+set -eux
+set -o pipefail
+command -v bc || dnf install -y bc
+command -v tput || dnf install -y ncurses
+PS4='$(tput setaf 4)$(printf "%-12s\\t%.3fs\\t@line\\t%-10s" $(date +%T) $(echo $(date "+%s.%3N")-'$(date "+%s.%3N")' | bc ) $LINENO)$(tput sgr
+0)'
 
 SSHD_CONFIG="/etc/ssh/sshd_config"
 
+if [ -f $SSHD_CONFIG ]; then
 # ensure that there is a trailing newline before attempting to concatenate
 sed -i -e '$a\' "$SSHD_CONFIG"
 
@@ -24,4 +33,5 @@ if grep -q -E "^[[:space:]]*GSSAPIAuthentication" "$SSHD_CONFIG"; then
     sed -i "s/^\s*GSSAPIAuthentication.*/${GSSAPI}/" "$SSHD_CONFIG"
 else
     echo "$GSSAPI" >>"$SSHD_CONFIG"
+fi
 fi
