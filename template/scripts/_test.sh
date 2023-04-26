d45ff784bbb1d6945a7c85be3349a0cfa00c73f4
Spencer Smolen <45985425+kriipke@users.noreply.github.com>
2023-04-25 22:51:54 -0400

    removed system configuration scripts from _test.sh

--- template/scripts/_test.sh
+++ template/scripts/_test.sh
@@ -2,21 +2,23 @@
 
 # ORDER SCRIPTS ARE TO BE RUN IN:
 
+set -ex
+
 #./motd.sh
 #./issue.sh
 #./metadata.sh
 #./sshd.sh
 #./networking.sh
-#./configure_vim.sh
-#./configure_zsh.sh
-#./install_fzf.sh
-#./install_nnn.sh
-#./configure_tmux.sh
-#./install_bat.sh
-#./install_croc.sh
-#./install_fd.sh
-#./install_lnav.sh
-#./install_rg.sh
-#./install_z.sh
+./configure_vim.sh
+./configure_zsh.sh
+./install_fzf.sh
+./install_nnn.sh
+./configure_tmux.sh
+./install_bat.sh
+./install_croc.sh
+./install_fd.sh
+./install_lnav.sh
+./install_rg.sh
+./install_z.sh
 #./cleanup.sh
 #./minimize.sh
