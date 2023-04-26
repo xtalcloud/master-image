830d835ba19ab939dcd0f07a9014e19d553fb2f7
Spencer Smolen <45985425+kriipke@users.noreply.github.com>
2023-04-25 22:47:54 -0400

    added debug configuration to install_z.sh

--- template/scripts/install_z.sh
+++ template/scripts/install_z.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh -x
+#!/usr/bin/sh
 
 #
 #  Install: z
@@ -16,17 +16,13 @@ PS4='$(tput setaf 4)$(printf "%-12s\\t%.3fs\\t@line\\t%-10s" $(date +%T) $(echo 
 
 Z_REPO_ROOT="https://raw.githubusercontent.com/rupa/z/v$Z_RELEASE"
 Z_TARGET_DIR=/usr/share/z
-TEST_CMD="test -f $HOME/.z"
 
 printf "Installing command: z (%s)\\n" "$Z_RELEASE"
 (
-	set -x
-	cd /tmp
-	mkdir -p $Z_TARGET_DIR
-	curl -Lo $Z_TARGET_DIR/z.sh $Z_REPO_ROOT/z.sh
-	curl -Lo /usr/share/man/man1/z\.1 $Z_REPO_ROOT/z.1
-	# current user
-	echo ". $Z_TARGET_DIR" >> $HOME/.zshrc
-	# /etc/skel
-	echo ". $Z_TARGET_DIR" >> /etc/skel/.zshrc
+        cd /tmp
+        mkdir -p $Z_TARGET_DIR
+        curl -Lo $Z_TARGET_DIR/z.sh $Z_REPO_ROOT/z.sh
+        curl -Lo /usr/share/man/man1/z\.1 $Z_REPO_ROOT/z.1
+        echo ". $Z_TARGET_DIR/z.sh" >> /etc/zshrc
 )
+echo 'Succesfully installed z!'
